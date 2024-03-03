395bdf7df16fda65fef8e4b2d2c077e9adf8ec00
Edouard Desgrée <edouard.desgree@gmail.com>
2024-03-03 16:38:20 +0100

    add audio data

---
+++ src/data.js
@@ -0,0 +1,44 @@
+
+export const ambianceData = [
+    {
+        id: 1,
+        name: 'Ambiance 1',
+        audiosId: [
+            1, 2, 3
+        ]
+    },
+    {
+        id: 2,
+        name: 'Ambiance 2',
+        audiosId: [
+            4, 5
+        ],
+    }
+];
+export const audios = [
+    {
+        id: 1,
+        name: 'Birds',
+        url: 'birds.mp3'
+    },
+    {
+        id: 2,
+        name: 'Wind',
+        url: 'wind.mp3'
+    },
+    {
+        id: 3,
+        name: 'Insects',
+        url: 'insects.mp3'
+    },
+    {
+        id: 4,
+        name: 'Waves',
+        url: 'waves.mp3'
+    },
+    {
+        id: 5,
+        name: 'Rain',
+        url: 'rain.mp3'
+    },
+];
