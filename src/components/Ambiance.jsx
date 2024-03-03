35697d6a5d59eeabacd67558e815eaecb9f0cc35
Edouard Desgrée <edouard.desgree@gmail.com>
2024-03-03 18:24:57 +0100

    audio styles

--- src/components/Ambiance.jsx
+++ src/components/Ambiance.jsx
@@ -1,14 +1,18 @@
+import styles from "./Ambiance.module.scss";
 import Audio from "./Audio";
 export default function Ambiance({ name, audios, ambianceId }) {
     const audioElements = audios.map((audio) => {
         return (
+
             <Audio key={audio.id} name={audio.name} file={audio.file} />
         );
     });
     return (
         <div title={name}>
             <h3>Ambiance {name}</h3>
-            {audioElements}
+            <div className={styles.audioList}>
+                {audioElements}
+            </div>
         </div>
     );
 }
--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Audio.module.scss";
 
 export default function Audio({ name, audioId, file }) {
     const [isPlaying, setIsPlaying] = React.useState(false);
-    const [volume, setVolume] = React.useState(0.5);
+    const [volume, setVolume] = React.useState(.5);
     const audioRef = React.useRef(null);
     const togglePlay = () => {
         if (isPlaying) {
@@ -17,19 +18,31 @@ export default function Audio({ name, audioId, file }) {
         setVolume(e.target.value);
         audioRef.current.volume = e.target.value;
     };
+
+
+    const MAX = 1;
+    const getBackgroundSize = () => {
+        return { backgroundSize: `${(volume * 100) / MAX}% 100%` };
+    };
+
     return (
         <div id={audioId}>
             <h5>{name}</h5>
             <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
-            <input
-                type="range"
-                min="0"
-                max="1"
-                step="0.01"
-                value={volume}
-                onChange={handleVolumeChange}
-            />
-            <audio ref={audioRef} controls={false} >
+            <div className={styles.slider}>
+                <input
+                    type="range"
+                    min="0"
+                    max={MAX}
+                    step="0.01"
+                    value={volume}
+                    onChange={handleVolumeChange}
+                    style={getBackgroundSize()}
+                    disabled={!isPlaying}
+                />
+            </div>
+
+            <audio ref={audioRef} controls={false} loop>
                 <source src={file} type="audio/mpeg" />
                 Your browser does not support the audio element.
             </audio>
