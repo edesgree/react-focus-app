f2d50d3d2a24c4ee280221287d991703cfb4bf2f
Edouard Desgrée <edouard.desgree@gmail.com>
2024-03-07 18:16:17 +0100

    miragejs remove environment dev local

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import App from './App.jsx';
 import './assets/styles.scss';
 import { makeServer } from './server.js';
 
-if (process.env.NODE_ENV === 'development') {
-    makeServer({ environment: 'development' }); // Initialize Mirage server in development environment
-}
+makeServer();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
